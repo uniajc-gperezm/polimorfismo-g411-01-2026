3220d8f25fe890cfb1737cb8a91ebbcb1be4d875
Raico <wiggins54626@gmail.com>
2026-03-17 19:21:48 -0500

    Se actualia codigo

--- src/main/java/com/uniajc/Perro.java
+++ src/main/java/com/uniajc/Perro.java
@@ -2,6 +2,14 @@ package com.uniajc;
 
 public class Perro extends Animal {
 
+    public void hacerSonido() {
+        System.out.println("El perro ladra");
+    }
+
+    public void mostrarTruco(){
+        System.out.println("El perro hace un truco");
+    }
+
     @Override
     public void comer() {
         System.out.println("El perro come croquetas");
